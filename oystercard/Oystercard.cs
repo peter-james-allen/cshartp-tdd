f6e8d30a999b98ca108b172ad65e7a44b84c1ea2
Pete <peter_james_allen@hotmail.com>
2021-06-28 16:23:33 +0100

    added TouchOut method

--- oystercard/Oystercard.cs
+++ oystercard/Oystercard.cs
@@ -1,17 +1,44 @@
+using System;
+
 namespace oystercard
 {
     public class Oystercard
     {
+        private const double MAXIMUM_BALANCE = 90.0;
         public double Balance { get; private set; }
+        public bool IsInJourney { get; private set; }
 
         public Oystercard(double balance)
         {
             Balance = balance;
+            IsInJourney = false;
         }
 
         public void TopUp(double amount)
         {
-            Balance += amount;
+            if ((Balance + amount) > MAXIMUM_BALANCE)
+            {
+                throw new ArgumentOutOfRangeException($"Maximum Limit of £{MAXIMUM_BALANCE} Reached.");
+            }
+            else
+            {
+                Balance += amount;
+            }
+        }
+
+        public void Deduct(double amount)
+        {
+            Balance -= amount;
+        }
+
+        public void TouchIn()
+        {
+            IsInJourney = true;
+        }
+
+        public void TouchOut()
+        {
+            IsInJourney = false;
         }
     }
 }
--- oystercard/OystercardTest.cs
+++ oystercard/OystercardTest.cs
@@ -1,27 +1,62 @@
 using NUnit.Framework;
 using oystercard;
+using System;
 
 namespace oystercardTest
 {
     public class Tests
     {
+        private Oystercard oystercard;
+        
         [SetUp]
         public void Setup()
         {
+            oystercard = new Oystercard(50.0);
         }
 
         [Test]
         public void NewOysterCardHasBalance()
         {
-            Oystercard oystercard = new Oystercard(10.0);
-            Assert.AreEqual(10.0, oystercard.Balance);
+            Assert.AreEqual(50.0, oystercard.Balance);
         }
 
         [Test]
         public void AddBalance()
         {
-            Oystercard oystercard = new Oystercard(10.0);
             oystercard.TopUp(10.0);
-            Assert.AreEqual(20.0, oystercard.Balance);
-        }    }
+            Assert.AreEqual(60.0, oystercard.Balance);
+
+            oystercard.TopUp(30.0);
+            Assert.AreEqual(90.0, oystercard.Balance);
+            Assert.Throws<ArgumentOutOfRangeException>(() => oystercard.TopUp(10.0));
+        }
+
+        [Test]
+        public void RemoveBalance()
+        {
+            oystercard.Deduct(10.0);
+            Assert.AreEqual(40.0, oystercard.Balance);
+        }
+
+       [Test]
+       public void InJourney()
+       {
+           Assert.AreEqual(false, oystercard.IsInJourney);
+       }
+
+       [Test]
+       public void TouchIn()
+       {
+           oystercard.TouchIn();
+           Assert.AreEqual(true, oystercard.IsInJourney);
+       }
+
+       [Test]
+       public void TouchOut()
+       {
+           oystercard.TouchIn();
+           oystercard.TouchOut();
+           Assert.AreEqual(false, oystercard.IsInJourney);
+       }
+    }
 }
